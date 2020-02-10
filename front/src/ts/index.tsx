b90d7daf564889630239df2a8cfb3cfcea96a92f
red2fred2 <admin@red2fred2.com>
2020-02-10 05:30:12 +0000

    Rearrange examples

--- front/src/ts/index.tsx
+++ front/src/ts/index.tsx
@@ -5,22 +5,15 @@ import * as ReactDOM from 'react-dom'
 
 //react elements
 import {ErrorBoundary} from './ErrorBoundary'
-import {TimeSlot} from './TimeSlot'
+import {Day} from './Day'
 
 //DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
 
-    const cb = (slot:number, state:string) => {
-        console.log(`slot number ${slot} is now in state ${state}`)
-    }
-
     //render react test element
     ReactDOM.render(
         <ErrorBoundary>
-            <TimeSlot slot={1} text="12:00am" defaultState={'Unselected'} onChange={cb}/>
-            <TimeSlot slot={2} text="12:20am" defaultState={'Unselected'} onChange={cb}/>
-            <TimeSlot slot={3} text="12:40am" defaultState={'Unselected'} onChange={cb}/>
-            <TimeSlot slot={4} text="1:00pm" defaultState={'Not-Available'} onChange={cb}/>
+            <Day date={new Date()} onChange={()=>{}}/>
         </ErrorBoundary>,
         document.getElementById('react-root')
     )
