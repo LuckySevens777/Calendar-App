981ef4438d485cb8dcd20c34acbdd3df5c333105
Dylan Polson <red2fred2>
2020-02-20 17:29:04 -0600

    Add basic root

--- webpage/front/src/ts/index.tsx
+++ webpage/front/src/ts/index.tsx
@@ -6,6 +6,7 @@ import * as ReactDOM from 'react-dom'
 //react elements
 import {ErrorBoundary} from './ErrorBoundary'
 import {Day} from './Day'
+import {SideBar} from './SideBar'
 
 //DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //render react test element
     ReactDOM.render(
-        <div>
+        /*<div>
             <ErrorBoundary>
                 <Day date={now} onChange={()=>{}}/>
             </ErrorBoundary>
@@ -33,6 +34,52 @@ document.addEventListener('DOMContentLoaded', () => {
             <ErrorBoundary>
                 <Day date={new Date(now.getFullYear(), now.getMonth(), now.getDate()+5)} onChange={()=>{}}/>
             </ErrorBoundary>
+        </div>*/
+        <div className="row">
+            <div className="center card-panel col s12 l10">
+            {/* <div className="container">
+                <div className="row">
+                    <div className="card">
+                        <div className="col s6">
+                            <div className="switch">
+                                <label>
+                                    Off
+                                    <input type="checkbox">
+                                    <span className="lever"></span>
+                                    On
+                                </label>
+                            </div>
+                        </div>
+                        <div className="col s6">
+                            <a onclick="M.toast({html: 'Whatever you just did worked!'})" className="btn blue">Display Confirmation</a>
+                        </div>
+
+                    </div>
+                </div>
+
+                    <div className="card">
+                        <ul className="collapsible">
+                            <li>
+                                <div className="collapsible-header hoverable row">
+                                    <a href="#!" className="col s12 grey-text text-darken-2"><span className="new badge red lighten-1">2</span>Event Invitations</a>
+                                </div>
+                                <div className="collapsible-body">
+                                    <div className="collection">
+                                        <a href="#!" className="collection-item blue-text">Event 1</a>
+                                        <a href="#!" className="collection-item blue-text">Event 2</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div> */}
+            </div>
+            <div className="card-panel col s12 l2">
+                <ErrorBoundary>
+                    <SideBar/>
+                </ErrorBoundary>
+            </div>
         </div>,
         document.getElementById('react-root')
     )
