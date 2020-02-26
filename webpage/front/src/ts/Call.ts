4b347202859519efcc54dd390775c5a73411df3b
Ross <rhcopeland101@gmail.com>
2020-02-26 11:21:21 -0600

    api connection now works

--- webpage/front/src/ts/Call.ts
+++ webpage/front/src/ts/Call.ts
@@ -14,7 +14,6 @@ export async function call(the_url) {
 	//console.log(content);
 }
 
-
 export class ApiCall {
 	public user: string = ""
 	public actions: string = ""
@@ -36,28 +35,31 @@ export class ApiCall {
 	}
 
 	makeBody() {
-		let body = "";
-		body += "{ ";
-		body += "'User':'" + this.user + "',";
-		body += "'Action':'" + this.actions + "', ";
-		body += "'Day' : '" + this.day + "', ";
-		body += "'Times' : '" + this.times + "',";
-		body += "'Event_name' : '" + this.event_name + "',";
-		body += "'Event_Description' : '" + this.event_description + "',";
-		body += "'Event_ID' : '" + this.event_id + "'}";
+		let body = '';
+		body += '{ ';
+		body += '"User":"' + this.user + '",';
+		body += '"Action":"' + this.actions + '", ';
+		body += '"Day" : [' + this.day + '], ';
+		body += '"Times" : [' + this.times + '],';
+		body += '"Event_name" : "' + this.event_name + '",';
+		body += '"Event_Description" : "' + this.event_description + '",';
+		body += '"Event_ID" : "' + this.event_id + '"}';
 		return(body);
 	}
 
-	async standardCall(the_body: string) {
+	async standardCall(the_body: any) {
 		let body_promise;
-		const the_resp = fetch('http://localhost/api', {
+		let testing = `{"User": "Drmk5","Action": "Get-All-Events","Day": [],"Times": [],"Event_Name": "","Event_Description": "","Event_ID": ""}`;
+	console.log(testing);
+		const the_resp = fetch('/api', {
 			//mode: 'no-cors',
 			method: 'POST',
 			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
+				'Accept': '*/*',
+				'Content-Type': 'application/json',
+				'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/80.0.3987.116 Safari/537.36'
 			},
-			body: JSON.stringify(the_body)
+			body: testing
 		}).then( data => {
 			data.body.getReader().read().then(res => {
 				body_promise = String.fromCharCode.apply(null, res.value);
@@ -67,28 +69,36 @@ export class ApiCall {
 		})
 	}
 
+	signUp(the_name:string) {
+		this.actions = "Sign-Up";
+		this.user = the_name;
+		let call_body = this.makeBody();
+		//console.log(call_body);
+		this.standardCall(JSON.parse(call_body));
+	}
+
 	getAllEvents() {
 		this.actions = "Get-All-Events";
 		let call_body = this.makeBody();
-		this.standardCall(call_body);
+		this.standardCall(JSON.parse(call_body));
 	}
 
 	getEventsAttending() {
 		this.actions = "Get-Events-Attending";
 		let call_body = this.makeBody();
-		this.standardCall(call_body);
+		this.standardCall(JSON.parse(call_body));
 	}
 
 	getEventsForDays(date:string) {
 		this.day = date;
 		let call_body = this.makeBody();
-		this.standardCall(call_body);
+		this.standardCall(JSON.parse(call_body));
 	}
 
 	getAttendees(id_of_event:string) {
 		this.event_id = id_of_event;
 		let call_body = this.makeBody();
-		this.standardCall(call_body);
+		this.standardCall(JSON.parse(call_body));
 	}
 
 
