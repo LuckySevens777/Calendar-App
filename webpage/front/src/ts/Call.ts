059c48f0a36c0ec125aef019d8cb76b4921f9c02
Dylan Polson <red2fred2>
2020-02-26 16:04:57 -0600

    Create slots to time string function

--- webpage/front/src/ts/Call.ts
+++ webpage/front/src/ts/Call.ts
@@ -48,6 +48,11 @@ export class ApiCall {
 		return(String.fromCharCode.apply(null, the_text.value));
 	}
 
+	private slots2timeString(slots:number[]) : string{
+		const map = ["01:00", "01:20", "01:40", "02:00", "02:20", "02:40", "03:00", "03:20", "03:40", "04:00", "04:20", "04:40", "05:00", "05:20", "05:40", "06:00", "06:20", "06:40", "07:00", "07:20", "07:40", "08:00", "08:20", "08:40", "09:00", "09:20", "09:40", "10:00", "10:20", "10:40", "11:00", "11:20", "11:40", "12:00", "12:20", "12:40", "13:00", "13:20", "13:40", "14:00", "14:20", "14:40", "15:00", "15:20", "15:40", "16:00", "16:20", "16:40", "17:00", "17:20", "17:40", "18:00", "18:20", "18:40", "19:00", "19:20", "19:40", "20:00", "20:20", "20:40", "21:00", "21:20", "21:40", "22:00", "22:20", "22:40", "23:00", "23:20", "23:40", "00:00", "00:20", "00:40"]
+		return '[' + slots.map(slot => map[slot-1]).join(',') + ']'
+	}
+
 	/**
 	 * Add user to an event
 	 * @param the_name the name of the event to join
@@ -102,8 +107,14 @@ export class ApiCall {
 	 * @param the_day the date formatted as ["YYYY", "MM", "DD"]
 	 * @param time_slots the array of time slot numbers to be created
 	 */
-	public createEvent(the_event_name: string, the_event_description: string, the_day: string[], time_slots: number[]) : void {
+	public async createEvent(the_event_name: string, the_event_description: string, the_day: string[], time_slots: number[]) : Promise<void> {
+		this.actions = "Create-Event"
+		this.event_name = the_event_name
+		this.event_description = the_event_description
+		this.day = `${the_day[1]}-${the_day[2]}-${the_day[0]}`
 
+		let call_body = this.makeBody
+		return await this.standardCall(call_body);
 	}
 
 	/**
